6c64e5542780290e58cc87251c2c6c78cc2ce952
piaic65617 <58557597+piaic65617@users.noreply.github.com>
2020-06-05 21:23:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        Content Updated by Muhammad Kamran
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-        </a>
-        Content Updated by Muhammad Kamran
+          Learn React
+        </a>      
       </header>
     </div>
   );
